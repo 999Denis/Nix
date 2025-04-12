628f04457412425f23921db311bb85a0cf6ab9dd
Denis <ravoiu.denis.g@gmail.com>
2025-04-13 01:32:45 +0200

    Add files via upload

---
+++ system/home.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  home.username = "denis";
+  home.homeDirectory = "/home/denis";
+  home.stateVersion = "24.11";
+
+  home.packages = with pkgs; [
+	micro
+	discord
+	inputs.zen-browser.packages."${system}".default
+  ];
+
+  home.sessionVariables = {
+    EDITOR = "micro";
+    BROWSER = "firefox";
+    GBM_BACKEND = "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+  };
+
+  programs.home-manager.enable = true;
+
+  
+  nixpkgs.config.allowUnfree = true;
+
+
+  systemd.user.startServices = "sd-switch";
+
+
+}
